d31c4ebd459af591ba35910cf695973429c63a7a
JeffBobGary21 <joshuaridley07@gmail.com>
2022-06-27 17:49:27 +1000

    Changed how buttons work, cause they were dumb before

--- Game Website/Games/fullscreen.js
+++ Game Website/Games/fullscreen.js
@@ -7,3 +7,12 @@ function makeUnfullScreen() {
     document.getElementsByTagName("iframe")[0].className = "unfullScreen";
     var elem = document.body;
 }
+
+document.getElementById("hideable").style.display ="none";
+
+function showHideable() {
+    document.getElementById("hideable").style.display = "block";
+}
+function hideHideable() {
+    document.getElementById("hideable").style.display ="none";
+}
