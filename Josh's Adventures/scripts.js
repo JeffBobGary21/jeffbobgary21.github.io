3be19b80a59a2890a3a7e787b321023b5b926d7c
JeffBobGary21 <joshuaridley07@gmail.com>
2022-07-04 20:37:21 +1000

    Added Dark Toggle And Other Things On Josh's Adventures

---
+++ Josh's Adventures/scripts.js
@@ -0,0 +1,14 @@
+// Dark Mode Switcher Button
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark_mode");
+
+}
+function toggleDarkModeText() {
+    var x = document.getElementById("toggleDarkModeText");
+    if (x.innerHTML === "light_mode") {
+      x.innerHTML = "dark_mode";
+    } else {
+      x.innerHTML = "light_mode";
+    }
+}
