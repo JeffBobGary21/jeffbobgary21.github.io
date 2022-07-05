bdd8d587f56a7d8777ce07be6a355db4512df4fc
JeffBobGary21 <joshuaridley07@gmail.com>
2022-07-05 15:16:41 +1000

    Dark mode cookie Attempt 1

--- Josh's Adventures/scripts.js
+++ Josh's Adventures/scripts.js
@@ -8,11 +8,10 @@ function toggleDarkModeText() {
     var x = document.getElementById("toggleDarkModeText");
     if (x.innerHTML === "light_mode") {
       x.innerHTML = "dark_mode";
-      document.cookie = "darkmode=true";
     } else {
       x.innerHTML = "light_mode";
-      document.cookie = "darkmode=false";
     }
 }
 
-document.cookie = "darkmode=true";
+// Dark Mode Cookie
+Cookie.set('dark-mode-enabled', 'true');
