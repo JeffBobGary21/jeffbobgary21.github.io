8e21329ce1c6c33a6cb8b59f32a912ba7dc9bb5c
JeffBobGary21 <joshuaridley07@gmail.com>
2022-07-06 10:35:54 +1000

    Dark Mode Cookie Finished. Completly working.

--- Josh's Adventures/scripts.js
+++ Josh's Adventures/scripts.js
@@ -1,14 +1,33 @@
 // Dark Mode Switcher Button
 function toggleDarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark_mode");
-
+  var element = document.body;
+  element.classList.toggle("dark_mode");
+  toggleDarkModeText()
 }
 function toggleDarkModeText() {
-    var x = document.getElementById("toggleDarkModeText");
-    if (x.innerHTML === "light_mode") {
-      x.innerHTML = "dark_mode";
-    } else {
-      x.innerHTML = "light_mode";
-    }
+  var x = document.getElementById("toggleDarkModeText");
+  if (x.innerHTML === "light_mode") {
+    x.innerHTML = "dark_mode";
+    setDarkModeFalse()
+  } else {
+    x.innerHTML = "light_mode";
+    setDarkModeTrue()
+  }
+}
+
+// Functions To Set Darkmode Status
+function setDarkModeTrue() {
+document.cookie = "darkmode=true"
+}
+
+function setDarkModeFalse() {
+  document.cookie = "darkmode=false"
+}
+
+// Get cookie value?
+function getDarkmodeCookie() {
+  var cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]:decodeURIComponent(value) }),{});
+  if (cookies.darkmode == "false") {
+    toggleDarkMode()
+  }
 }
