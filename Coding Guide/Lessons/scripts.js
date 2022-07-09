4324e70293f4508f86eec9e61983838e7a2adc10
JeffBobGary21 <joshuaridley07@gmail.com>
2022-07-09 15:14:59 +1000

    Unfinished Changes

--- Coding Guide/Lessons/scripts.js
+++ Coding Guide/Lessons/scripts.js
@@ -4,30 +4,30 @@ function toggleDarkMode() {
     element.classList.toggle("dark_mode");
     toggleDarkModeText()
   }
-  function toggleDarkModeText() {
-    var x = document.getElementById("toggleDarkModeText");
-    if (x.innerHTML === "light_mode") {
-      x.innerHTML = "dark_mode";
-      setDarkModeFalse()
-    } else {
-      x.innerHTML = "light_mode";
-      setDarkModeTrue()
-    }
+function toggleDarkModeText() {
+  var x = document.getElementById("toggleDarkModeText");
+  if (x.innerHTML === "light_mode") {
+    x.innerHTML = "dark_mode";
+    setDarkModeFalse()
+  } else {
+    x.innerHTML = "light_mode";
+    setDarkModeTrue()
   }
+}
   
-  // Functions To Set Darkmode Status
-  function setDarkModeTrue() {
-  document.cookie = "darkmode=true"
-  }
+// Functions To Set Darkmode Status
+function setDarkModeTrue() {
+document.cookie = "darkmode=true"
+}
   
-  function setDarkModeFalse() {
-    document.cookie = "darkmode=false"
-  }
+function setDarkModeFalse() {
+  document.cookie = "darkmode=false"
+}
   
-  // Get cookie value?
-  function getDarkModeCookie() {
-    var cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]:decodeURIComponent(value) }),{});
-    if (cookies.darkmode == "false") {
-      toggleDarkMode()
-    }
+// Get cookie value
+function getDarkModeCookie() {
+  var cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]:decodeURIComponent(value) }),{});
+  if (cookies.darkmode == "false") {
+    toggleDarkMode()
   }
+}
