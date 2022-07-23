1c622d3d2b6d182ed33d45afa40cfb8326c9b9c9
JeffBobGary21 <joshuaridley07@gmail.com>
2022-07-23 20:05:14 +1000

    Added Dark Mode To Main Site

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// Dark Mode Switcher Button
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark_mode");
+    toggleDarkModeText()
+}
+function toggleDarkModeText() {
+  var x = document.getElementById("toggleDarkModeText");
+  if (x.innerHTML === "light_mode") {
+    x.innerHTML = "dark_mode";
+    setDarkModeFalse()
+  } else {
+    x.innerHTML = "light_mode";
+    setDarkModeTrue()
+  }
+}
+  
+// Functions To Set Darkmode Status
+function setDarkModeTrue() {
+document.cookie = "darkmode=true";
+document.getElementById('logo').src='Main Site Resources/equinoxx.xyz(BIG LIGHT).png';
+}
+  
+function setDarkModeFalse() {
+  document.cookie = "darkmode=false";
+  document.getElementById('logo').src='Main Site Resources/equinoxx.xyz(BIG DARK).png';
+}
+  
+// Get cookie value?
+function getDarkModeCookie() {
+  var cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]:decodeURIComponent(value) }),{});
+  if (cookies.darkmode == "false") {
+    toggleDarkMode();
+  }
+}
+  
+// Get cookie value on page load
+window.onload = function() {
+  getDarkModeCookie()
+}
+
+// Change Logo
